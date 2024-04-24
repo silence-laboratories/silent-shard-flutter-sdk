1636bc774527caa6fc138f96882622b4a16a5f84
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-04-24 13:59:40 +0700

    feat(local_database): Use Map<String, WalletBackup> in local database to manage multiple wallet backup

--- lib/src/storage/local_database.dart
+++ lib/src/storage/local_database.dart
@@ -3,12 +3,10 @@
 
 import 'dart:convert';
 
+import 'package:dart_2_party_ecdsa/dart_2_party_ecdsa.dart';
 import 'package:sodium/sodium.dart';
 
 import 'storage.dart';
-import '../types/pairing_data.dart';
-import '../types/keyshare.dart';
-import '../types/wallet_backup.dart';
 import '../ctss_bindings_generated.dart';
 
 class LocalDatabase {
@@ -18,9 +16,9 @@ class LocalDatabase {
 
   PairingData? _pairingData;
   Map<String, List<Keyshare2>> _keyshares;
-  WalletBackup _walletBackup;
+  Map<String, WalletBackup> _walletBackups;
 
-  LocalDatabase(this._storage, this._pairingData, this._keyshares, this._walletBackup, [bool saveOnCreation = true]) {
+  LocalDatabase(this._storage, this._pairingData, this._keyshares, this._walletBackups, [bool saveOnCreation = true]) {
     if (saveOnCreation) {
       saveToStorage();
     }
@@ -31,7 +29,7 @@ class LocalDatabase {
   factory LocalDatabase.fromStorage(Sodium sodium, CTSSBindings ctss, Storage storage) {
     PairingData? pairingData;
     Map<String, List<Keyshare2>> keyshares = {};
-    WalletBackup? walletBackup;
+    Map<String, WalletBackup> walletBackup = {};
 
     final data = storage.getString(storageKey);
     if (data != null) {
@@ -40,20 +38,20 @@ class LocalDatabase {
         pairingData = PairingData.fromJson(sodium, json['pairingData']);
         keyshares =
             json['keyshares']?.map<Keyshare2>((key, value) => MapEntry(key, (value as List).map((e) => Keyshare2.fromBytes(ctss, e)).toList()));
-        walletBackup = WalletBackup.fromJson(json['backup']);
+        walletBackup = json['backup']?.map<WalletBackup>((key, value) => MapEntry(key, WalletBackup.fromJson(value)));
       } catch (e) {
         print('Failed to load local state: $e');
       }
     }
 
-    return LocalDatabase(storage, pairingData, keyshares, walletBackup ?? WalletBackup(), false);
+    return LocalDatabase(storage, pairingData, keyshares, walletBackup, false);
   }
 
   void saveToStorage() {
     final json = {
       'pairingData': _pairingData?.toJson(),
       'keyshares': _keyshares.map((key, value) => MapEntry(key, value.map((e) => e.toBytes()).toList())),
-      'backup': _walletBackup.toJson(),
+      'backup': _walletBackups.map((key, value) => MapEntry(key, value.toJson())),
     };
     _storage.setString(storageKey, jsonEncode(json));
   }
@@ -108,10 +106,47 @@ class LocalDatabase {
 
   // --- Wallet Backup ---
 
-  WalletBackup get walletBackup => _walletBackup;
+  Map<String, WalletBackup> get walletBackups => _walletBackups;
 
-  set walletBackup(WalletBackup? backup) {
-    _walletBackup = backup ?? WalletBackup();
+  set walletBackups(Map<String, WalletBackup> backups) {
+    _walletBackups = backups;
+    saveToStorage();
+  }
+
+  void addAccount(String walletName, AccountBackup backup) {
+    if (_walletBackups.containsKey(walletName)) {
+      _walletBackups[walletName]!.addAccount(backup);
+    } else {
+      _walletBackups[walletName] = WalletBackup([backup]);
+    }
+    saveToStorage();
+  }
+
+  void addAccounts(String walletName, Iterable<AccountBackup> backups) {
+    if (_walletBackups.containsKey(walletName)) {
+      _walletBackups[walletName]!.addAccounts(backups);
+    } else {
+      _walletBackups[walletName] = WalletBackup(backups);
+    }
+    saveToStorage();
+  }
+
+  void removeAccount(String walletName, AccountBackup backup) {
+    if (_walletBackups.containsKey(walletName)) {
+      _walletBackups[walletName]!.removeAccount(backup);
+    }
+    saveToStorage();
+  }
+
+  void removeAccountAt(String walletName, int index) {
+    if (_walletBackups.containsKey(walletName)) {
+      _walletBackups[walletName]!.removeAccountAt(index);
+    }
+    saveToStorage();
+  }
+
+  void removeAllBackups() {
+    _walletBackups.clear();
     saveToStorage();
   }
 }
