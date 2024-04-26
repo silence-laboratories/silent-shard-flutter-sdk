6680ca3f7ba35d5bfdc940789a01d2150face7e1
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-04-26 21:25:35 +0700

    feat(fetchRemoteBackup): Bring back fetchRemoteBackup api to be compatible with snap wallet

--- lib/src/actions/remote_backup_action.dart
+++ lib/src/actions/remote_backup_action.dart
@@ -7,6 +7,38 @@ import '../types/pairing_data.dart';
 import '../transport/messages/backup_message.dart';
 import '../transport/shared_database.dart';
 
+class FetchRemoteBackupAction {
+  final SharedDatabase _sharedDatabase;
+  final PairingData _pairingData;
+  StreamSubscription<BackupMessage>? _streamSubscription;
+
+  final _completer = Completer<String>();
+
+  FetchRemoteBackupAction(this._sharedDatabase, this._pairingData);
+
+  Future<String> start() async {
+    _streamSubscription = _sharedDatabase
+        .backupUpdates(_pairingData.pairingId)
+        .timeout(const Duration(seconds: 60))
+        .listen(_handleMessage, onError: _handleError, cancelOnError: true);
+    return _completer.future;
+  }
+
+  void cancel() {
+    _streamSubscription?.cancel();
+  }
+
+  void _handleMessage(BackupMessage message) {
+    cancel();
+    _sharedDatabase.deleteBackupMessage(_pairingData.pairingId);
+    _completer.complete(message.backupData);
+  }
+
+  void _handleError(Object error) {
+    _completer.completeError(error);
+  }
+}
+
 class RemoteBackupListener {
   final SharedDatabase _sharedDatabase;
   final PairingData _pairingData;
