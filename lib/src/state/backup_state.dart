41a481f77c72c89963f484427fdbd813c9cef257
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-05-08 17:28:42 +0700

    feat(backup_state): Add, replace, remove accounts from wallet backup

--- lib/src/state/backup_state.dart
+++ lib/src/state/backup_state.dart
@@ -19,23 +19,18 @@ class BackupState extends ChangeNotifier {
     notifyListeners();
   }
 
-  void replaceAccount(String walletId, AccountBackup backup) {
-    _database.replaceAccount(walletId, backup);
+  void addBackupAccount(String walletId, AccountBackup backup) {
+    _database.addBackupAccount(walletId, backup);
     notifyListeners();
   }
 
   void replaceAccounts(String walletId, Iterable<AccountBackup> backups) {
-    _database.replaceAccounts(walletId, backups);
+    _database.replaceAllBackupAccounts(walletId, backups);
     notifyListeners();
   }
 
-  void removeAccount(String walletId, AccountBackup backup) {
-    _database.removeAccount(walletId, backup);
-    notifyListeners();
-  }
-
-  void removeAccountAt(String walletId, int index) {
-    _database.removeAccountAt(walletId, index);
+  void removeBackupAccountBy(String walletId, String address) {
+    _database.removeBackupAccountBy(walletId, address);
     notifyListeners();
   }
 
